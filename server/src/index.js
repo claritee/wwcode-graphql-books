bc66717dbe05a7c42febf42b75cc31a70c6c1253
Claire Tran <c.tran83@gmail.com>
2018-11-20 22:14:00 +1100

    Adding mocklist

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,14 @@
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer, MockList } = require('apollo-server');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 
 const mocks = {
-  Int: () => 10,
+  Int: () => 1,
   Float: () => 22.5,
   String: () => 'this value is a mock',
+  Query: () =>({
+    books: () => new MockList([1, 10]),
+  }),
 };
 
 const server = new ApolloServer({
