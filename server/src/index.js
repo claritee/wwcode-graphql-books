0bd75472c44b559e87a4558310555c8db3852889
Claire Tran <c.tran83@gmail.com>
2018-11-20 21:43:29 +1100

    Adding mocks

--- server/src/index.js
+++ server/src/index.js
@@ -2,9 +2,16 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 
+const mocks = {
+  Int: () => 10,
+  Float: () => 22.5,
+  String: () => 'this value is a mock',
+};
+
 const server = new ApolloServer({
 	typeDefs,
 	resolvers,
+  mocks
 });
 
 server.listen().then(({ url }) => {
